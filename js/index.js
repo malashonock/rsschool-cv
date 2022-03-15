d51079856553e3670eb0d3d3fa14e9e2c6554aa7
Yan Malashonak <malashonock@gmail.com>
2022-03-15 23:35:08 +0300

    feat: update content

--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
-const links = [...document.querySelectorAll('.navbar a')];
-const mediaQueryDesktop = window.matchMedia('(min-width: 48.001rem)');
-const burgerMenu = document.querySelector('.burger-menu');
-const burgerMenuIcon = burgerMenu.querySelector('span');
-const navMenu = document.querySelector('.navlinks');
+const links = [...document.querySelectorAll(".navbar a")];
+const mediaQueryDesktop = window.matchMedia("(min-width: 48.001rem)");
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerMenuIcon = burgerMenu.querySelector("span");
+const navMenu = document.querySelector(".navlinks");
 
 links.forEach((link) => {
-    console.log(link);
-    link.addEventListener('click', () => toggleNavMenu());
+  console.log(link);
+  link.addEventListener("click", () => toggleNavMenu());
 });
 
-mediaQueryDesktop.addEventListener('change', (mediaQuery) => {
-    if (mediaQuery.matches) {
-        restoreDesktopNavmenu();
-    } else {
-        hideNavmenu();
-    }
+mediaQueryDesktop.addEventListener("change", (mediaQuery) => {
+  if (mediaQuery.matches) {
+    restoreDesktopNavmenu();
+  } else {
+    hideNavmenu();
+  }
 });
 
 const toggleNavMenu = () => {
-    if (getComputedStyle(burgerMenu).getPropertyValue('display') === 'none') {
-        return;
-    }
+  if (getComputedStyle(burgerMenu).getPropertyValue("display") === "none") {
+    return;
+  }
 
-    if (getComputedStyle(navMenu).getPropertyValue('display') === 'block') {
-        navMenu.style.display = 'none';
-        setBurgerMenuIcon('burger');
-    } else {
-        navMenu.style.display = 'block';
-        setBurgerMenuIcon('close');
-    }
+  if (getComputedStyle(navMenu).getPropertyValue("display") === "block") {
+    navMenu.style.display = "none";
+    setBurgerMenuIcon("burger");
+  } else {
+    navMenu.style.display = "block";
+    setBurgerMenuIcon("close");
+  }
 };
 
 const restoreDesktopNavmenu = () => {
-    navMenu.style.display = 'flex';    
-    setBurgerMenuIcon('close');
+  navMenu.style.display = "flex";
+  setBurgerMenuIcon("close");
 };
 
 const hideNavmenu = () => {
-    navMenu.style.display = 'none';    
-    setBurgerMenuIcon('burger');
+  navMenu.style.display = "none";
+  setBurgerMenuIcon("burger");
 };
 
 const setBurgerMenuIcon = (iconType) => {
-    switch (iconType) {
-        case 'burger':
-            burgerMenuIcon.className = 'fa fa-bars';
-            break;
-        case 'close':
-            burgerMenuIcon.className = 'fa fa-times';
-            break;
-    }
+  switch (iconType) {
+    case "burger":
+      burgerMenuIcon.className = "fa fa-bars";
+      break;
+    case "close":
+      burgerMenuIcon.className = "fa fa-times";
+      break;
+  }
 };
